44784c0c9f92dcb25f0084fb117bec62a52a5572
Agnes Monica Sanjani Harefa <112601993+agnesmosh241104@users.noreply.github.com>
2024-12-19 06:02:36 +0700

    model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,30 +1,44 @@
 <?php
 
+// app/Models/User.php
+
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
-
+    use Notifiable, HasApiTokens, HasFactory;
+    
     protected $table = 'users';
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'email', 'password', 'status', 'photo', 'role', 
+
+        // ,'google_refresh_token' ,'google_id', 'google_token',
     ];
 
-    /**
+    public function files()
+    {
+        return $this->hasMany(File::class);
+    }
+
+    public function getPhotoUrlAttribute()
+    {
+        return $this->photo 
+            ? asset('storage/' . $this->photo) 
+            : asset('assets/images/default-profile.jpg');
+    }
+    
+      /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
